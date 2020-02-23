9158aef73eab5d663884cc05831bc1cd623837d7
Matthias Westhues <westhues.matthias@gmail.com>
2020-02-23 12:02:12 +0100

    square root calculation

---
+++ fundamentals/sqrt.py
@@ -0,0 +1,17 @@
+import numpy as np
+import typing
+
+def sqrt(value: float) -> float:
+    if (value < 0):
+        return np.nan
+    
+    err: float = 1e-15
+    t: float = value
+    while abs(t - value/t) > err * t:
+        t = (value/t + t) / 2.0
+    return t
+
+
+
+if __name__ == '__main__':
+    sqrt()
